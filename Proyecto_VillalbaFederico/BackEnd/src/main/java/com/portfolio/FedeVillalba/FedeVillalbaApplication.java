d0fda40a8e9f61a8afae89432077ea307d1452f5
fingesell <97805854+fingesell@users.noreply.github.com>
2022-07-25 12:04:12 -0300

    cambios 25/7

---
+++ Proyecto_VillalbaFederico/BackEnd/src/main/java/com/portfolio/FedeVillalba/FedeVillalbaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.FedeVillalba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedeVillalbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FedeVillalbaApplication.class, args);
+	}
+
+}
