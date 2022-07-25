257c5230d28e45bcafe03511000a69e55c4259a0
fingesell <97805854+fingesell@users.noreply.github.com>
2022-07-25 13:33:35 -0300

    cambios 25/7 mediodia

--- Proyecto_VillalbaFederico/BackEnd/src/main/java/com/portfolio/FV/FvApplication.java
+++ Proyecto_VillalbaFederico/BackEnd/src/main/java/com/portfolio/FV/FvApplication.java
@@ -1,13 +1,13 @@
-package com.portfolio.FedeVillalba;
+package com.portfolio.FV;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FedeVillalbaApplication {
+public class FvApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FedeVillalbaApplication.class, args);
+		SpringApplication.run(FvApplication.class, args);
 	}
 
 }
--- Proyecto_VillalbaFederico/BackEnd/src/test/java/com/portfolio/FV/FvApplicationTests.java
+++ Proyecto_VillalbaFederico/BackEnd/src/test/java/com/portfolio/FV/FvApplicationTests.java
@@ -1,10 +1,10 @@
-package com.portfolio.FedeVillalba;
+package com.portfolio.FV;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FedeVillalbaApplicationTests {
+class FvApplicationTests {
 
 	@Test
 	void contextLoads() {
