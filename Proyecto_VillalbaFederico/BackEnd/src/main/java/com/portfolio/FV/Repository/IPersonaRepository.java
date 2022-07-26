d63c27d35d8eb3347b4579152dc8016eed56576d
fingesell <97805854+fingesell@users.noreply.github.com>
2022-07-25 23:44:03 -0300

    cambios 25/7 noche

---
+++ Proyecto_VillalbaFederico/BackEnd/src/main/java/com/portfolio/FV/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.FV.Repository;
+
+import com.portfolio.FV.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
